9787cb1eeb7d65667a6f074cb49b1fe6ed4852c4
tahtty <aahtty@espol.edu.ec>
2017-02-26 03:10:44 -0500

    correccion de errores de tipeo en el crud

--- routes/cursos.js
+++ routes/cursos.js
@@ -29,16 +29,16 @@ router.get('/todos',function(req, res, next){
 router.post('/agregar', function(req, res, next) {
   	var profesor = req.body.profesor;
 	var paralelo = req.body.paralelo;
-	var estudiante = req.body.estudiante;
+	var estudiantes = req.body.estudiantes;
 
 	console.log(profesor)
 	console.log(paralelo)
-	console.log(estudiante)
+	console.log(estudiantes)
 
 		// Validation
 	req.checkBody('profesor', 'profesor requerido').notEmpty();
 	req.checkBody('paralelo', 'paralelo requerido').notEmpty();
-	req.checkBody('estudiante', 'estudiante requerido').notEmpty();
+	req.checkBody('estudiantes', 'estudiante requerido').notEmpty();
 
 
 
@@ -60,15 +60,25 @@ router.post('/agregar', function(req, res, next) {
 		Curso.createCurso(nuevocurso, function(err, curso){
 			if(err) throw err;
 			console.log(curso);
+			
 		});
-
-		req.flash('success_msg', 'El curso se ha registrado');
-
 		res.redirect('/cursos');
+		req.flash('success_msg', 'El curso se ha registrado');
 	}
 });
 
-router.get('/:paralelo'),function(req,res,next){
+router.get('/paralelo/:paralelo'),function(req,res,next){
+	var paralelo=req.body.paralelo;
+	var respuesta=Curso.getCursoByParalelo(paralelo,function(err,user){
+		if(err) throw err;
+		console.log(user);
+	});
+	var respuesta={}
+	respuesta.paralelo=paralelo;
+
+}
+
+router.get('/profesor/:profesor'),function(req,res,next){
 	var paralelo=req.body.paralelo;
 	var respuesta=Curso.getCursoByParalelo(paralelo,function(err,user){
 		if(err) throw err;
